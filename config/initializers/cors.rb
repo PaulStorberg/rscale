8bcd9ddbd2115545a9a0f9c238225c8d11ed5c3f
Paul Storberg <paul@storberg.net>
2024-10-28 22:06:40 -0400

    edits

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
+# config/initializers/cors.rb
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://r-scale.com',
-            'https://www.r-scale.com'
+    origins 'https://r-scale.com', 'https://www.r-scale.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
